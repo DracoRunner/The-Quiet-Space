32bcd7aba4c826614e24f9febfafa207cf39df63
Mahendra <mahendra.baghel@accedo.tv>
2025-10-19 17:17:55 +0800

    added host url

--- services/blogService.ts
+++ services/blogService.ts
@@ -1,9 +1,7 @@
 import type { Blog } from "##/types/BlogType";
 
 export async function getBlogs(): Promise<Blog[]> {
-  const base =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    `http://localhost:${process.env.PORT ?? 3000}`;
+  const base = process.env.NEXT_PUBLIC_SITE_URL;
   const url = new URL("/api/blogs", base).toString();
   const res = await fetch(url, { next: { revalidate: 0 }, cache: "no-store" });
   if (!res.ok) return [];
